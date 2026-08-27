bf55c842d63c5bbebb50d7efcaa73061710e6f44
Jose Calembe <josecalembe2@gmail.com>
2026-08-27 13:46:16 +0530

    atualizando o desafio_95

--- 03 - Mundo3/aula_19/desafio_95.py
+++ 03 - Mundo3/aula_19/desafio_95.py
@@ -1,5 +1,26 @@
 jogadores=list()
-gols=[]
+total = 0
 atleta=dict()
-atleta['Nome']=input("Nome do jogador:")
-atleta['Partida']=int(input("Quantas partidas o atleta['Nome'] jogou??"))
+while True:
+    gols = []
+
+    total=0
+    atleta['Nome'] = input("Nome do jogador:")
+    atleta['Partida'] = int(input(f'Quantas partidas o {atleta["Nome"]} jogou?'))
+    for c in range(1,atleta['Partida']+1):
+        gols.append(int(input(f'Quantos gols na {c} partida?')))
+    atleta['Gols'] = gols
+    atleta['Total'] = sum(gols)
+    jogadores.append(atleta.copy())
+    gols.clear()
+    resp=str(input("Quer continuar? [S/N]")).upper().strip()[0]
+    while resp not in 'SN':
+        resp=str(input("Quer continuar? [S/N]")).upper().strip()
+    if resp=='N':
+       break
+print('COD NOME GOLS TOTAL')
+
+
+
+
+
