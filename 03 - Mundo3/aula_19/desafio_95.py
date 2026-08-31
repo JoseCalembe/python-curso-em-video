349bb56f3e1b5010ea573bafe8bf266746070157
Jose Calembe <josecalembe2@gmail.com>
2026-08-31 13:34:09 +0530

    atualizando o desafio 95

--- 03 - Mundo3/aula_19/desafio_95.py
+++ 03 - Mundo3/aula_19/desafio_95.py
@@ -1,15 +1,15 @@
 jogadores=list()
-total = 0
 atleta=dict()
+gols = list()
+
 while True:
-    gols = []
 
     total=0
     atleta['Nome'] = input("Nome do jogador:")
     atleta['Partida'] = int(input(f'Quantas partidas o {atleta["Nome"]} jogou?'))
     for c in range(1,atleta['Partida']+1):
         gols.append(int(input(f'Quantos gols na {c} partida?')))
-    atleta['Gols'] = gols
+    atleta['Gols'] = gols.copy()
     atleta['Total'] = sum(gols)
     jogadores.append(atleta.copy())
     gols.clear()
@@ -19,6 +19,16 @@ while True:
     if resp=='N':
        break
 print('COD NOME GOLS TOTAL')
+for pos,atleta in enumerate(jogadores):
+    print(f'{pos}{atleta["Nome"]} {atleta["Gols"]} {atleta["Total"]}')
+print()
+print('-='*30)
+while True:
+    n=int(input("Mostrar dados de qual jogador?"))
+    if 0<n<=len(jogadores):
+        for partidas in (jogadores[n]):
+            for golos in partidas:
+                print(f'No jogo {partidas} fez {golos} gols')
 
 
 
