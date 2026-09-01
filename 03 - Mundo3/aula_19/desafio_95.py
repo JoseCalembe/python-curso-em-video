9563cbd59f6a7ce9af3ca44d28ace620d225a104
Jose Calembe <josecalembe2@gmail.com>
2026-09-01 13:48:15 +0530

    Atualizando o desafio 95

--- 03 - Mundo3/aula_19/desafio_95.py
+++ 03 - Mundo3/aula_19/desafio_95.py
@@ -25,10 +25,17 @@ print()
 print('-='*30)
 while True:
     n=int(input("Mostrar dados de qual jogador?"))
-    if 0<n<=len(jogadores):
-        for partidas in (jogadores[n]):
-            for golos in partidas:
-                print(f'No jogo {partidas} fez {golos} gols')
+
+    if n<0 or n>len(jogadores):
+        if n!=99:
+           print("Erro! nao existe jogador com o codigo {}! Tente novamente".format(n))
+    elif n>=0 and n<=len(jogadores):
+         print(f'LEVANTAMENTO DO JOGADOR {jogadores[n]["Nome"]}')
+         for partidas, golos in enumerate(jogadores[n]['Gols']):
+             print(f'No jogo {partidas+1} fez {golos} gols')
+    if n==99:
+            break
+print("<<<ENCERRADO>>>")
 
 
 
